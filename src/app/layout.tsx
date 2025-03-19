f88bc429f0fa08611e6b876fce5f7f364aeb7fba
kelvintryanto <kelvin.tryanto@gmail.com>
2025-03-19 19:46:26 +0700

    fix logo in browser tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import { Toaster } from '@/components/ui/toaster';
-import type { Metadata, Viewport } from 'next';
-import './globals.css';
+import { Toaster } from "@/components/ui/toaster";
+import type { Metadata, Viewport } from "next";
+import "./globals.css";
 
 export const viewport: Viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1,
   minimumScale: 1,
   maximumScale: 5,
   userScalable: true,
-  viewportFit: 'cover',
-  interactiveWidget: 'resizes-visual',
+  viewportFit: "cover",
+  interactiveWidget: "resizes-visual",
 };
 
 export const metadata: Metadata = {
-  title: 'TerrariaVet | Rumah Terraria',
-  description: 'Merawat dengan Cinta, Menjaga dengan Keahlian',
-  icons: '/logo_white.png',
+  title: "TerrariaVet | Rumah Terraria",
+  description: "Merawat dengan Cinta, Menjaga dengan Keahlian",
+  icons: "/logo.png",
 };
 
 export default function RootLayout({
